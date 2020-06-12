f37e27908185f59bfb92c5aac897c79836c303c7
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-06-12 14:23:35 +0200

    create reducer and component

---
+++ src/components/LightContainer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const mapStateToProps = state => ({
+  count: state
+});
+
+const CountComponent = ({ count, dispatch }) => (
+  <div>
+    <p>{count}</p>
+    <button
+      onClick={() => dispatch({ type: 'ADD' })}
+    >
+      + 1
+    </button>
+    <button
+      onClick={() => dispatch({ type: 'REMOVE' })}
+    >
+      - 1
+    </button>
+    <button
+      onClick={() => dispatch({ type: 'ADD10' })}
+    >
+      + 10
+    </button>
+    <button
+      onClick={() => dispatch({ type: 'REMOVE10' })}
+    >
+      - 10
+    </button>
+    <button
+      onClick={() => dispatch({ type: 'RESET' })}
+    >
+      reset
+    </button>
+  </div>
+);
+
+export default connect(
+  mapStateToProps
+)(CountComponent);
